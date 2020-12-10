cf2633aa9560b3c27a8354a98cd14802c1247e81
Will-Gon <will.colocho@gmail.com>
2020-12-10 14:51:13 -0800

    commit tree cc148f3f33190a726eb0ff201f44b848a0941af9

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,30 @@
 // your Bomb code here!
+import React, { Component } from 'react'
+
+class Bomb extends Component {
+    constructor(props) {
+        super()
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+
+    // generateCountdown = () => {
+    //     if (this.state.secondsLeft <= 0) {
+    //         return 'Boom!'
+    //     }else{
+    //         (this.state.secondsLeft)
+    //     }
+    //}
+
+    render() {
+        const SECONDS_LEFT = this.state.secondsLeft
+        return(
+            <div>
+                {this.state.secondsLeft < 1 ?  "Boom!" : `${this.state.secondsLeft} seconds left before I go boom!`}
+            </div>
+        )
+    }
+}
+
+export default Bomb;
